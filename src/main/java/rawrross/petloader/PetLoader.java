963252671ce1bde86d2613b1a822fe0aef343cd6
RawrRoss <randyross2000@gmail.com>
2022-07-10 19:48:37 -0700

    Template setup

---
+++ src/main/java/rawrross/petloader/PetLoader.java
@@ -0,0 +1,13 @@
+package rawrross.petloader;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PetLoader implements ModInitializer {
+
+    public static final String MODID = "petloader";
+
+    @Override
+    public void onInitialize() {
+    }
+
+}
